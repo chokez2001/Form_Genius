c56f6700e356ce004fb7df0e3b1d1530f963d58b
chokez2001 <diegojosueandrade@gmail.com>
2023-07-20 21:51:25 -0500

    parece que ya sirve super bien el form fill, nada seguro

--- dist/assets/md.transition-legacy-f25aac16.js
+++ dist/assets/md.transition-legacy-f25aac16.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-cc634623.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
+System.register(["./index-legacy-4f191a76.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- src/pages/fill_form.tsx
+++ src/pages/fill_form.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef} from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonTextarea, IonDatetime, IonCheckbox, IonButton } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonTextarea, IonDatetime, IonCheckbox, IonButton, InputChangeEventDetail } from '@ionic/react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { guardarFormularioLleno } from '../models/dababase'; // Import the guardarFormularioLleno function
 
@@ -12,50 +12,92 @@ const DetalleFormularioPage: React.FC = () => {
   const history = useHistory();
   const { formularioSeleccionado } = location.state || { formularioSeleccionado: null };
   const [formularioLleno, setFormularioLleno] = useState<any | null>(null);
+  const [camposList, setCamposList] = useState<string[]>([]);
+  const [nuevoNombre, setNuevoNombre] = useState<string>("");
 
-  // Use useRef to create a mutable reference to the form data
-  const formularioLlenoRef = useRef<any | null>({
-    ...formularioSeleccionado,
-    campos: Object.keys(formularioSeleccionado.campos).reduce((acc: any, campo) => {
-      acc[campo] = { ...formularioSeleccionado.campos[campo], value: '' };
-      return acc;
-    }, {})
-  });
-
-  if (!formularioSeleccionado) {
-    return <div>Formulario no encontrado.</div>;
-  }
+  useEffect(() => {
+    if (formularioSeleccionado) {
+      // Create a list of all the field keys (campos) from the selected form and store the form data
+      const campos = formularioSeleccionado.campos;
+      const camposKeys = Object.keys(campos);
+      setCamposList(camposKeys);
+      setFormularioLleno(formularioSeleccionado);
+    }
+  }, [formularioSeleccionado]);
 
   const handleChange = (campo: string, value: any) => {
-    formularioLlenoRef.current.campos[campo].value = value;
+    setFormularioLleno((prevFormulario: { campos: { [x: string]: any; }; }) => ({
+      ...prevFormulario,
+      campos: {
+        ...prevFormulario.campos,
+        [campo]: {
+          ...prevFormulario.campos[campo],
+          value: value
+        }
+      }
+    }));
+  };
+
+  const handleNuevoNombreChange = (e: CustomEvent<InputChangeEventDetail>) => {
+    setNuevoNombre(e.detail.value || ""); // Establecer un valor predeterminado para evitar 'undefined'
   };
 
   const handleSubmit = async () => {
     try {
+      if (!nuevoNombre) {
+        console.error("Por favor, ingrese un nombre para el formulario antes de guardarlo.");
+        return;
+      }
+
+      setFormularioLleno((prevFormulario: any) => ({
+        ...prevFormulario,
+        Nombre: nuevoNombre
+      }));
+
+      // Verificar que ningún campo esté vacío
+      const camposLlenos = Object.keys(formularioLleno?.campos || {}).every((campo) => {
+        return formularioLleno?.campos[campo]?.value !== "";
+      });
+
+      if (!camposLlenos) {
+        console.error("Por favor, llene todos los campos antes de guardar el formulario.");
+        return;
+      }
+
       // Call the function to save the filled form
-      await guardarFormularioLleno(formularioLlenoRef.current);
+      await guardarFormularioLleno(formularioLleno);
 
       // Navigate back to the previous page or any other desired destination
-      history.push('/pages/empty_forms');
+      history.push('/pages/fill');
     } catch (error) {
       console.error('Error al guardar el formulario lleno:', error);
     }
   };
+  
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Detalle del Formulario</IonTitle>
+          <IonTitle>
+            <IonInput
+              value={nuevoNombre}
+              placeholder="Nuevo nombre"
+              onIonChange={handleNuevoNombreChange}
+            />
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonList>
-          {Object.keys(formularioLlenoRef.current.campos).map((campo, index) => {
-            const campoConfig = formularioLlenoRef.current.campos[campo];
+          {camposList.map((campo, index) => {
+            const campoConfig = formularioLleno?.campos[campo];
+            if (!campoConfig) return null;
+
             return (
               <IonItem key={index}>
                 <IonLabel position="floating">{campo}</IonLabel>
+                {/* Render different input components based on the tipo of the field */}
                 {campoConfig.tipo === 'short_text' && (
                   <IonInput type="text" value={campoConfig.value} onIonChange={(e) => handleChange(campo, e.detail.value)} />
                 )}
@@ -63,11 +105,11 @@ const DetalleFormularioPage: React.FC = () => {
                   <IonTextarea value={campoConfig.value} onIonChange={(e) => handleChange(campo, e.detail.value)} />
                 )}
                 {campoConfig.tipo === 'date_picker' && (
-                  <IonDatetime value={campoConfig.value} onIonChange={(e) => handleChange(campo, e.detail.value)} />
+                  <IonInput type="date" value={campoConfig.value} onIonChange={(e) => handleChange(campo, e.detail.value)} />
                 )}
                 {campoConfig.tipo === 'checkbox' && (
                   <>
-                    {campoConfig.opciones.split(',').map((opcion: string, index: React.Key | null | undefined) => (
+                    {campoConfig.opciones.split(',').map((opcion: string, index: number) => (
                       <IonItem key={index}>
                         <IonLabel>{opcion}</IonLabel>
                         <IonCheckbox
@@ -99,7 +141,6 @@ const DetalleFormularioPage: React.FC = () => {
       </IonContent>
     </IonPage>
   );
-  
 };
 
 export default DetalleFormularioPage;
